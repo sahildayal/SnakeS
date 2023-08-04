69f6f28393b45b561da34c70c715d04fe25f8de9
Sahil Dayal <sd8804@rit.edu>
2023-08-04 07:31:59 -0400

    Creating game screen

--- main.py
+++ main.py
@@ -40,5 +40,22 @@ direction = 'down'
 label = Label(window, text="Score:{}".format(score), font=('consolas', 35))
 label.pack()
 
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width = GAME_WIDTH)
+canvas.pack() 
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2)-(window_width/2))
+y = int((screen_height/2)-(window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
